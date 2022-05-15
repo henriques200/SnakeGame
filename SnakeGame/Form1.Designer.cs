100cf75e8cea618159ab5a0d56294863e5824053
Emanuel Henriques <emanuel.h200@hotmail.com>
2022-05-15 12:59:50 +0100

    Basic UI changes.

--- SnakeGame/Form1.Designer.cs
+++ SnakeGame/Form1.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_txt_1 = new System.Windows.Forms.Label();
+            this.label_score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(378, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_txt_1
+            // 
+            this.label_txt_1.AutoSize = true;
+            this.label_txt_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_txt_1.Location = new System.Drawing.Point(414, 13);
+            this.label_txt_1.Name = "label_txt_1";
+            this.label_txt_1.Size = new System.Drawing.Size(90, 20);
+            this.label_txt_1.TabIndex = 1;
+            this.label_txt_1.Text = "Pontuação:";
+            // 
+            // label_score
+            // 
+            this.label_score.AutoSize = true;
+            this.label_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_score.Location = new System.Drawing.Point(510, 13);
+            this.label_score.Name = "label_score";
+            this.label_score.Size = new System.Drawing.Size(29, 20);
+            this.label_score.TabIndex = 2;
+            this.label_score.Text = "----";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(622, 450);
+            this.Controls.Add(this.label_score);
+            this.Controls.Add(this.label_txt_1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_txt_1;
+        private System.Windows.Forms.Label label_score;
     }
 }
 
